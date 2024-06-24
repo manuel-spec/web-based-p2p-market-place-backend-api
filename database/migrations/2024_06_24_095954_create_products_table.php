cbfce85f6948cb98799a6a005094425d04d393aa
manuel-spec <amaniase084@gmail.com>
2024-06-24 13:04:17 +0300

    products

---
+++ database/migrations/2024_06_24_095954_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->string('image');
+            $table->foreignId('user_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\authController;
 
-Route::prefix('api')->group(function () {
-    Route::post('register', [authController::class, 'register']);
-    Route::post('login', [authController::class, 'login']);
+Route::prefix('api')->group(function() {
+    Route::prefix('auth')->group(function () {
+        Route::post('register', [authController::class, 'register']);
+        Route::post('login', [authController::class, 'login']);
+    });
 });
